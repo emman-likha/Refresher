c1b0ff7b21523c2f5d4c61810afb3a57da961ac5
bulaonjohnemm4nuel.21@gmail.com <bulaonjohnemm4nuel.21@gmail.com>
2025-07-30 00:40:43 +0800

    feat: add Footer component and integrate it into App

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} John Emmanuel Bulaon. All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer
