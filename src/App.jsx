24ca426c672344c722fd337f4ab58bd49a66aea8
bulaonjohnemm4nuel.21@gmail.com <bulaonjohnemm4nuel.21@gmail.com>
2025-08-05 23:45:51 +0800

    feat: remove unused components and add Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import Header from "./Header"
-import Footer from "./Footer"
-import Food from "./Food"
+
 
 function App() {
-  return(
-    <>
-      <Header></Header>
-      <Food></Food>
-      <Footer></Footer>
-      </>
-  )
-
+  
 }
 
 export default App
---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+
+
+function Card(){
+    return(
+        <div className="card">
+            <img src="" alt="" />
+        </div>
+    )
+}
+
+export default Card
